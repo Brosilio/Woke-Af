5eaac0c52acfb9d8e94b64443372a44a9fc535c4
Brosilio <brosilio@youshouldfear.me>
2020-09-17 21:29:38 -0400

    pushed project files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Woke-Af
 Portable, native PowerShell module for sending Wake-On-LAN packets
+
+# Usage
+```powershell
+Send-Wol -mac AA:BB:CC:DD:EE:FF
+```
---
+++ src/WolPSCmdlet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace Woke_Af
+{
+    [Cmdlet(VerbsCommunications.Send,"Wol")]
+    [OutputType(typeof(string))]
+    public class WolPSCmdlet : PSCmdlet
+    {
+        [Parameter(ParameterSetName = "mac", Mandatory = true, Position = 0)]
+        public string MacAddress { get; set; }
+
+        [Parameter]
+        public int Port { get; set; } = 0;
+
+        protected override void ProcessRecord()
+        {
+            MacAddress = MacAddress.Replace(':', '-');
+            PhysicalAddress mac = PhysicalAddress.Parse(MacAddress);
+
+            UdpClient client = new UdpClient()
+			{
+				EnableBroadcast = true
+			};
+
+            List<byte> magic = new List<byte>(102) { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+            for (int i = 0; i < 16; i++)
+                magic.AddRange(mac.GetAddressBytes());
+
+            int sent = client.Send(magic.ToArray(), magic.Count, new IPEndPoint(IPAddress.Broadcast, Port));
+
+            WriteVerbose($"Broadcasted {sent} bytes. Recipient: {MacAddress} on port {Port}");
+
+            client.Dispose();
+		}
+    }
+}
