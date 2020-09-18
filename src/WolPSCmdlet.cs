0a81203122031c1a373a349142ea0a6b63ca21d1
Brosilio <brosilio@youshouldfear.me>
2020-09-17 21:31:03 -0400

    fixed whitespace

--- src/WolPSCmdlet.cs
+++ src/WolPSCmdlet.cs
@@ -8,35 +8,35 @@ using System.Net.Sockets;
 
 namespace Woke_Af
 {
-    [Cmdlet(VerbsCommunications.Send,"Wol")]
-    [OutputType(typeof(string))]
-    public class WolPSCmdlet : PSCmdlet
-    {
-        [Parameter(ParameterSetName = "mac", Mandatory = true, Position = 0)]
-        public string MacAddress { get; set; }
+	[Cmdlet(VerbsCommunications.Send,"Wol")]
+	[OutputType(typeof(string))]
+	public class WolPSCmdlet : PSCmdlet
+	{
+		[Parameter(ParameterSetName = "mac", Mandatory = true, Position = 0)]
+		public string MacAddress { get; set; }
 
-        [Parameter]
-        public int Port { get; set; } = 0;
+		[Parameter]
+		public int Port { get; set; } = 0;
 
-        protected override void ProcessRecord()
-        {
-            MacAddress = MacAddress.Replace(':', '-');
-            PhysicalAddress mac = PhysicalAddress.Parse(MacAddress);
+		protected override void ProcessRecord()
+		{
+			MacAddress = MacAddress.Replace(':', '-');
+			PhysicalAddress mac = PhysicalAddress.Parse(MacAddress);
 
-            UdpClient client = new UdpClient()
+			UdpClient client = new UdpClient()
 			{
 				EnableBroadcast = true
 			};
 
-            List<byte> magic = new List<byte>(102) { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
-            for (int i = 0; i < 16; i++)
-                magic.AddRange(mac.GetAddressBytes());
+			List<byte> magic = new List<byte>(102) { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+			for (int i = 0; i < 16; i++)
+				magic.AddRange(mac.GetAddressBytes());
 
-            int sent = client.Send(magic.ToArray(), magic.Count, new IPEndPoint(IPAddress.Broadcast, Port));
+			int sent = client.Send(magic.ToArray(), magic.Count, new IPEndPoint(IPAddress.Broadcast, Port));
 
-            WriteVerbose($"Broadcasted {sent} bytes. Recipient: {MacAddress} on port {Port}");
+			WriteVerbose($"Broadcasted {sent} bytes. Recipient: {MacAddress} on port {Port}");
 
-            client.Dispose();
+			client.Dispose();
 		}
-    }
+	}
 }
